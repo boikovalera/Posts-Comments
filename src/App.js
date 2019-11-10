5bf5016b88216975994c5a9eba6bba5bd3b80c90
Бойко Валерий Михайлович <boikovalera@ukr.net>
2019-11-10 22:49:11 +0200

    Start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header/Header'
+import Posts from './components/Posts/Posts'
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Header></Header>
+        <Switch>
+          <Route path='/posts' component={Posts} />          
+          <Route path='/' render={() => <Redirect to="/posts" />} />
+        </Switch>
+      </div>      
+    </Router>
   );
 }
-
-export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="row">
+            <nav>
+                <div className="nav-wrapper">
+                    <div className="col s12">
+                        <span className="brand-logo">User Posts</span>                        
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
